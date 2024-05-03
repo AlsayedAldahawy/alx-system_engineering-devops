0289787ce42716eed04e13be599d2a24a0346bca
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-05-03 08:56:41 +0300

    rev-REGEX0608

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 
-puts ARGV[0].scan(/.+from:(\+?\w+)\].+to:(\+?\w+)\].+flags:(.+\d)\]/).join","
+puts ARGV[0].scan(/.+from:(\+?\w+)\].+to:(\+?\w+)\].+flags:([-110:]{5,15})\]/).join","
