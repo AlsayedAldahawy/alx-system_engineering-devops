86d0754613ad11a7bad57d36dc979845b62e57b3
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-05-13 22:42:23 +0300

    task-0

--- 0x06-regular_expressions/100-test.sh
+++ 0x06-regular_expressions/100-test.sh
@@ -6,7 +6,9 @@ strings=("Feb 1 11:00:00 ip-10-0-0-11 mdr: 2016-02-01 11:00:00 Receive SMS [SMSC
 
 "Feb 1 11:00:00 ip-10-0-64-11 mdr: 2016-02-01 11:00:00 Sent SMS [SMSC:SYBASE1] [SVC:backendtextme] [ACT:] [BINF:] [FID:] [from:18572406905] [to:14022180266] [flags:-1:0:-1:-1:-1] [msg:136:Far out in the uncharted backwaters of the unfashionable end of the western spiral arm of the Galaxy lies a small unregarded yellow sun.] [udh:0:]"
 
-"Feb 1 11:00:00 ip-10-0-64-11 mdr: 2016-02-01 11:00:00 Sent SMS [SMSC:SYBASE1] [SVC:backendtextme] [ACT:] [BINF:] [FID:] [from:12392190384] [to:19148265919] [flags:-1:0:-1:-1:-1] [msg:99:life forms are so amazingly primitive that they still think digital watches are a pretty neat idea.] [udh:0:]" "Feb 1 11:05:55 ip-10-0-0-11 mdr: 2016-02-01 11:05:55 Receive SMS [SMSC:SYBASE1] [SVC:] [ACT:] [BINF:] [FID:] [from:+17654650874] [to:+15635593023] [flags:-1:0:-1:0:-1] [msg:99:life forms are so amazingly primitive that they still think digital watches are a pretty neat idea.] [udh:6:0500035B0201]")
+"Feb 1 11:00:00 ip-10-0-64-11 mdr: 2016-02-01 11:00:00 Sent SMS [SMSC:SYBASE1] [SVC:backendtextme] [ACT:] [BINF:] [FID:] [from:12392190384] [to:19148265919] [flags:-1:0:-1:-1:-1] [msg:99:life forms are so amazingly primitive that they still think digital watches are a pretty neat idea.] [udh:0:]"
+
+"Feb 1 11:05:55 ip-10-0-0-11 mdr: 2016-02-01 11:05:55 Receive SMS [SMSC:SYBASE1] [SVC:] [ACT:] [BINF:] [FID:] [from:+17654650874] [to:+15635593023] [flags:-1:0:-1:0:-1] [msg:99:life forms are so amazingly primitive that they still think digital watches are a pretty neat idea.] [udh:6:0500035B0201]")
 
 for i in "${strings[@]}"
 do
