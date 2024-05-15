fa47556a869ae103c0c1ef76f23e47971ef10e35
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-05-15 08:00:48 +0300

    rev-DevOps-0A01

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,10 @@
-#  install flask from pip3.
+# Using Puppet, install flask from pip3
+package { 'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
+}
 
-package{ 'flask':
-        ensure   => '2.1.0',
-        provider => 'pip3'
+package { 'Werkzeug':
+    ensure   => '2.1.1',
+    provider => 'pip3',
 }
