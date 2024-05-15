bd56bf0cc8eb1736af05f3b19cc926bd27e300ee
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-05-15 18:01:01 +0300

    0x0A-configuration_management update

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -7,10 +7,16 @@ exec { 'kill_killmenow':
 # The path parameter specifies where to find the pkill command (common locations like /usr/bin and /bin)
 
 
-# Another Solution
+# Another Solutions
 #
 # exec { 'kill_killmenow':
 #   command  => 'pkill killmenow',
 #   provider => 'shell',
 # }
 # The provider was set to 'shell', which means the command is executed in a shell environment.
+
+# or
+# exec { 'killmenow':
+#     command => '/usr/bin/pkill killmenow',
+# }
+
