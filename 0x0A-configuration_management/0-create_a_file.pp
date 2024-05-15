afd04d090723f87674de9a23b02bc2269b40a6d1
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-05-15 07:34:59 +0300

    0x0A-configuration_management update

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # creates a file in /tmp
 
-file{'/tmp/school',
-    ensure => 'true',
-    mode => '0744',
-    owner => 'www-data',
-    group => 'www-data',
-    content => 'I love Puppet'
+file { '/tmp/school':
+  ensure  => present,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
