fe80a673baafe0f1553f2c05f64a5e40cf38c43c
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-05-16 03:37:49 +0300

    rev-DevOps-0B04

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,9 @@
+# Seting ssh client config file
+# must be configured to use the private key ~/.ssh/school
+# must be configured to refuse to authenticate using a password
+
 file { 'shh_config':
   ensure  => file,
   path    => '/home/alsayed/.ssh/ssh_config',
-  content => " Host *\n   PasswordAuthentication no\n",
+  content => " Host *\n   PasswordAuthentication no\n   IdentifyFile ~/.ssh/school\n",
 }
