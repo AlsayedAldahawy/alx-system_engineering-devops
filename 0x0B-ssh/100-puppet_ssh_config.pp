e06a1ec71e9c99ba27098adda8da9c6968e84918
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-05-16 04:08:05 +0300

    rev-DevOps-0B04 #Puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -8,7 +8,7 @@ file { '/etc/ssh/ssh_config':
   ensure => present,
 }
 
--> file_line { 'shh_config':
+file_line { 'shh_config':
   path => '/etc/ssh/ssh_config',
   line => "   PasswordAuthentication no\n   IdentifyFile ~/.ssh/school\n",
 }
