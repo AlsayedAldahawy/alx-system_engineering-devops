7d7322b544be24ee525a1ea27aaa374422aa91d7
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-05-27 00:53:38 +0300

    rev-DevOps-0x15-00

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -33,26 +33,26 @@ Note:
 - Replace <employee_id> with the actual employee ID you want to check.
 """
 
-if __name__ == "__main__"
-# Import necessary modules
-import requests
-from sys import argv
+if __name__ == "__main__":
+    # Import necessary modules
+    import requests
+    from sys import argv
 
-# Base URL for the REST API
-url = "https://jsonplaceholder.typicode.com/"
+    # Base URL for the REST API
+    url = "https://jsonplaceholder.typicode.com/"
 
-# Fetch user information based on the provided user ID
-user = requests.get(url + "users/" + argv[1]).json()
+    # Fetch user information based on the provided user ID
+    user = requests.get(url + "users/" + argv[1]).json()
 
-# Fetch TODO list for the same user
-todos = requests.get(url + "todos", params={'userId': argv[1]}).json()
+    # Fetch TODO list for the same user
+    todos = requests.get(url + "todos", params={'userId': argv[1]}).json()
 
-# Filter completed tasks
-complete = [c.get("title") for c in todos if c.get('completed') is True]
+    # Filter completed tasks
+    complete = [c.get("title") for c in todos if c.get('completed') is True]
 
-# Print summary of completed tasks
-print("Employee {} is done with tasks ({}/{}):".format(user.get('name'),
-                                                       len(complete),
-                                                       len(todos)))
-for title in complete:
-    print("\t{}".format(title))
+    # Print summary of completed tasks
+    print("Employee {} is done with tasks ({}/{}):".format(user.get('name'),
+                                                           len(complete),
+                                                           len(todos)))
+    for title in complete:
+        print("\t{}".format(title))
