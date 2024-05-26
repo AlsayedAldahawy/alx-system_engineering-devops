82c3465a79401d0d8cd644dbe2e1cd281941d3dc
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-05-26 21:40:19 +0300

    rev-DevOps-0x15-00

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,43 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
-import requests
-import sys
+# Python script that, using REST API, for a given employee ID
+# returns information about his/her TODO list progress.
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+    import requests
+    from sys import argv
+
+    argc = len(argv)
+    user_id = argv[1]
+    NUMBER_OF_DONE_TASKS = 0
+    TOTAL_NUMBER_OF_TASKS = 0
+    compleated_tasks = []
+
+    url = "https://jsonplaceholder.typicode.com/users/" + user_id
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()  # Parse JSON response
+        EMPLOYEE_NAME = data.get('name')
+        print(data.get('name'))
+    else:
+        print(f"Error: {response.status_code}")
+
+    url = 'https://jsonplaceholder.typicode.com/todos'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()  # Parse JSON response
+
+        for i in data:
+            if i.get('userId') == int(argv[1]):
+                TOTAL_NUMBER_OF_TASKS += 1
+                if i.get('completed'):
+                    NUMBER_OF_DONE_TASKS += 1
+                    compleated_tasks.append(i.get('title'))
+
+        print("Employee {} is done with tasks({}/{})".format(EMPLOYEE_NAME,
+              NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+        for i in compleated_tasks:
+            print("\t" + i)
+
+    else:
+        print(f"Error: {response.status_code}")
