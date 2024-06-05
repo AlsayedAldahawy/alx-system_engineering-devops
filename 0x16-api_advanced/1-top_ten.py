85e19a3f456462b54ae11b101744a75db37b44c3
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-06-05 03:22:32 +0300

    0x16-api_advanced update

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
-"""top ten"""
+"""
+Function that queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit.
+"""
 
 import requests
 
 
 def top_ten(subreddit):
-    """prints the titles of the first 10 hot posts"""
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
-               AppleWebKit/537.36 (KHTML, like Gecko)\
-               Chrome/124.0.0.0 Safari/537.36'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json().get('data').get('children')
-        for post in data[:10]:
-            print(post.get('data').get('title'))
+    """
+    Function that queries the Reddit API
+    - If not a valid subreddit, print None.
+    """
+    req = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+        params={"limit": 10},
+    )
+
+    if req.status_code == 200:
+        for get_data in req.json().get("data").get("children"):
+            dat = get_data.get("data")
+            title = dat.get("title")
+            print(title)
     else:
-        return None
+        print(None)
